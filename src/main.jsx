8350e49d0d5b0e4af9f624e7fbe9f805c1569c9c
Angga Cipta Triyana <anggaciptatriyana.r22mi@plb.ac.id>
2026-08-14 20:26:42 +0700

    update_company

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./dist/css/main.css";
+import "./main.css";
 import ScrollToTop from "./components/ScrollTopTop.jsx";
 import "animate.css";
 
