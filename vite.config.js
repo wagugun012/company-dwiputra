ae8cc5899af0dd8fea6a25b0ed7694a0fa7ac044
Angga Cipta Triyana <anggaciptatriyana.r22mi@plb.ac.id>
2026-08-14 19:47:23 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Mengizinkan semua host (termasuk Ngrok) untuk mengakses server ini
-    allowedHosts: true,
-  },
+  base: "/company-dwiputra", // Replace with your desired base path
 });
